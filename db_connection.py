1ec36f241403d5c15680bedb089e5a10e762ca37
tamara <tamara.malysheva@saritasa.com>
2020-09-24 21:19:44 +0700

    improve db connection for SQLite

--- db_connection.py
+++ db_connection.py
@@ -2,27 +2,36 @@ import sqlite3
 
 from exceptions import DatabaseAppError
 
-DEFAULT_CONNECTION = ':memory:'
+DEFAULT_SQLITE_CONNECTION = ':memory:'
+# define in memory connection for SQLite
+in_memory_sqlite_connection = sqlite3.connect(DEFAULT_SQLITE_CONNECTION)
 
 
 class AbstractDbConnection:
     """Abstract class for connect to Database and Execute SQL commands"""
 
-    def run(self, query, connection_params=None):
-        raise NotImplemented()
+    def run(self, query: str, connection_params=None):
+        """Connect and execute SQL query"""
+        raise NotImplementedError()
 
 
 class SQLiteConnection(AbstractDbConnection):
+    """Connection for SQLite"""
 
-    def run(self, query, connection_params=None, ) -> tuple:
+    def run(self, query: str, connection_params=None, ) -> tuple:
         try:
-            conn = sqlite3.connect(connection_params or DEFAULT_CONNECTION)
-            cursor = conn.execute("SELECT id, name  FROM django_migrations")
+
+            conn = sqlite3.connect(connection_params) if connection_params else in_memory_sqlite_connection
+            cursor = conn.execute(query)
             data = cursor.fetchall()
-            columns = [description[0] for description in cursor.description]
-            conn.close()
+
+            columns = []
+            if cursor.description:
+                columns = [description[0] for description in cursor.description]
+
             return data, columns
         except sqlite3.DatabaseError as e:
+            conn.close()
             raise DatabaseAppError(msg=e.args[0])
 
 
